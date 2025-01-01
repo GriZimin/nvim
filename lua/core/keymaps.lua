801c9feeb4952b362af7445a6727fceaa8a129d6
GriZimin <grizimin@gmail.com>
2025-01-01 20:08:53 +0500

    Noice and some other plugins added

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -4,6 +4,23 @@ local keymap = vim.keymap
 
 keymap.set("i", "jk", "<ESC>")
 
+-- Splitting windows
+keymap.set("n", "<leader>sv", "<C-w>v", { desc = "Split verticaly" })
+keymap.set("n", "<leader>sh", "<C-w>s", { desc = "Split horizontaly" })
+keymap.set("n", "<leader>se", "<C-w>=", { desc = "Make windows equal size" })
+keymap.set("n", "<leader>sx", "<cmd>close<CR>", {desc = "Close window" })
+keymap.set("n", "<leader>sm", "<cmd>MaximizerToggle<CR>", { desc = "Maximize/minimize a split" })
+
+-- Windows navigation
+keymap.set("n", "<C-j>", "<C-w>j")
+keymap.set("n", "<C-k>", "<C-w>k")
+keymap.set("n", "<C-h>", "<C-w>h")
+keymap.set("n", "<C-l>", "<C-w>l")
+
+-- Quit
+keymap.set("n", "<leader>x", "<cmd>x<cr>", { noremap = true, silent = true, desc = "Save and Close"})
+keymap.set("n", "<leader>Q", "<cmd>xa<cr>", { noremap = true, silent = true, desc = "Save and Close All"})
+
 -- Disable highlighting
 keymap.set("n", "<leader>nh", "<cmd>nohl<CR>")
 
---
+++ lua/plugins/extra.lua
@@ -0,0 +1,9 @@
+return {
+  { "andweeb/presence.nvim" },
+  {
+    "ray-x/lsp_signature.nvim",
+    event = "BufRead",
+    config = function() require("lsp_signature").setup() end,
+  },
+  { "MunifTanjim/nui.nvim" }
+}
